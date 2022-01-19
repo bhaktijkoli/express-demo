3cd2638c622ac6b06716a61c406f3f661d8ae2be
Bhaktij Koli <bhaktijkoli121@gmail.com>
2022-01-20 03:22:11 +0530

    chore: post comment, like, unlike api

--- src/http/controllers/post.controller.ts
+++ src/http/controllers/post.controller.ts
@@ -2,9 +2,13 @@ import db from '@db'
 import { Request, Response } from '@types'
 
 const NOT_AUTHORIZED = 'You are not authorized to perform this action'
+const ALREADY_LIKED = 'You have already liked this post'
+const NOT_LIKED = 'You have not liked this post before'
 
 const add = async (req: Request, res: Response) => {
+  // Get Title, Description from Body
   const { title, description } = req.body
+  // Create Post
   const data = await db.post.create({
     data: {
       title,
@@ -16,13 +20,21 @@ const add = async (req: Request, res: Response) => {
 }
 
 const remove = async (req: Request, res: Response) => {
+  // Check if Post Exists
   const { id } = req.params
   const post = await db.post.findFirst({
     where: { id }
   })
+  if (post === null) {
+    res.status(404)
+    return
+  }
+  // Check if User is the owner of the post
   if (post.userId !== req.ctx.user.id) {
     res.status(400).json({ message: NOT_AUTHORIZED })
+    return
   }
+  // Delete the Post
   await db.post.delete({
     where: {
       id
@@ -31,7 +43,98 @@ const remove = async (req: Request, res: Response) => {
   res.status(200).json({ data: post })
 }
 
+const like = async (req: Request, res: Response) => {
+  // Find Post
+  const { id } = req.params
+  const post = await db.post.findFirst({
+    where: { id }
+  })
+  if (post === null) {
+    res.status(404)
+    return
+  }
+  // Check if user has already liked the post
+  const alreadyLiked = await db.like.findFirst({
+    where: {
+      postId: post.id,
+      userId: req.ctx.user.id
+    }
+  })
+
+  if (alreadyLiked !== null) {
+    res.status(400).json({ message: ALREADY_LIKED })
+  }
+  // Create Like
+  await db.like.create({
+    data: {
+      postId: post.id,
+      userId: req.ctx.user.id
+    }
+  })
+  res.status(200).json({ success: true })
+}
+
+const unlike = async (req: Request, res: Response) => {
+  // Find Post
+  const { id } = req.params
+  const post = await db.post.findFirst({
+    where: { id }
+  })
+  if (post === null) {
+    res.status(404)
+    return
+  }
+  // Check if user has already liked the post
+  const alreadyLiked = await db.like.findFirst({
+    where: {
+      postId: post.id,
+      userId: req.ctx.user.id
+    }
+  })
+
+  if (alreadyLiked === null) {
+    res.status(400).json({ message: NOT_LIKED })
+  }
+
+  // Delete Like
+  await db.like.delete({
+    where: {
+      postId_userId: {
+        postId: post.id,
+        userId: req.ctx.user.id
+      }
+    }
+  })
+  res.status(200).json({ success: true })
+}
+
+const comment = async (req: Request, res: Response) => {
+  // Find Post
+  const { id } = req.params
+  const post = await db.post.findFirst({
+    where: { id }
+  })
+  if (post === null) {
+    res.status(404)
+    return
+  }
+  // Get Comment from Body
+  const { comment } = req.body
+  // Create Comment
+  const data = await db.comment.create({
+    data: {
+      message: comment,
+      postId: post.id,
+      userId: req.ctx.user.id
+    }
+  })
+  res.status(200).json({ data })
+}
+
 export default {
   add,
-  remove
+  remove,
+  like,
+  unlike,
+  comment
 }
--- src/http/routes/post.routes.ts
+++ src/http/routes/post.routes.ts
@@ -5,4 +5,7 @@ import authMiddleware from '@middlewares/auth.middleware'
 export default (app: Express) => {
   app.post('/api/posts', [authMiddleware], postController.add)
   app.delete('/api/posts/:id', [authMiddleware], postController.remove)
+  app.post('/api/like/:id', [authMiddleware], postController.like)
+  app.post('/api/unlike/:id', [authMiddleware], postController.unlike)
+  app.post('/api/comment/:id', [authMiddleware], postController.comment)
 }

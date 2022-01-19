b043bf2bcae0486cbe83ebfd3ad173cfaef7c72d
Bhaktij Koli <bhaktijkoli121@gmail.com>
2022-01-19 09:24:05 +0530

    chore: added error handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response, NextFunction } from 'express'
+import path from 'path'
 import Logger from '@utils/logger.utils'
+import createError from 'http-errors'
 
 const start = () => {
   // Create Express instance
@@ -8,6 +10,10 @@ const start = () => {
   // Middlwares
   app.use(express.json())
   app.use(express.urlencoded({ extended: false }))
+
+  // View Engine Setup
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'hbs')
 
   // Base Route
   app.get('/', (_: Request, res: Response) => {
@@ -21,6 +27,22 @@ const start = () => {
   // Start Listening
   app.listen(port, () => {
     Logger.debug(`App listening at http://localhost:${port}`)
+  })
+
+  // Catch 404 and forward to error handler
+  app.use((_: Request, __: Response, next: NextFunction) => {
+    next(createError(404))
+  })
+
+  // Error Handler
+  app.use((err: any, req: Request, res: Response, _: NextFunction) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message
+    res.locals.error = process.env.NODE_ENV === 'development' ? err : {}
+
+    // render the error page
+    res.status(err.status || 500)
+    res.render('error')
   })
 }
 
